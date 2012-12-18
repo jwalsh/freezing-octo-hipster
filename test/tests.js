977947a75018ee75f3e065ba55e3ea260ff614e4
Jason Walsh <jwalsh@bluekai.com>
2012-12-17 18:10:12 -0800

    Adding in unit tests for the build process

--- test/tests.js
+++ test/tests.js
@@ -3,3 +3,10 @@ module('core');
 test('hello test', function() {
   ok(1 == '1', 'Passed!');
 });
+
+
+test(
+  'version',
+  function() {
+    ok(typeof version !== 'undefined', 'Library is versioned');
+  });
